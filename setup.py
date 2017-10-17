d840860642eaf7dcbf415144c3ba74f7c3a2e9bb
Bernhard Mueller <bernhard.mueller11@gmail.com>
2017-10-17 20:54:55 +0700

    Update laser

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ The easiest way to use LASER is by installing Mythril command line tool:
 setup(
     name='laser-ethereum',
 
-    version='0.1.1',
+    version='0.1.2',
 
     description='Symbolic Ethereum virtual machine',
     long_description=long_description,
