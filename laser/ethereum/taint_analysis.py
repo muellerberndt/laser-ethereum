5e3f2aeddf0500b1478adcceeb774316aa2b8541
Joran Honig <joran.honig@gmail.com>
2018-05-05 14:00:00 +0200

    setup initial classes

---
+++ laser/ethereum/taint_analysis.py
@@ -0,0 +1,17 @@
+class TaintRecord:
+    """
+    TaintRecord contains tainting information for a specific (state, node)
+    """
+    pass
+
+
+class TaintRunner:
+    """
+    Taint runner, is able to run taint analysis on symbolic execution result
+    """
+    pass
+
+
+class TaintResult:
+    """ Taint analysis result obtained after having ran the taint runner"""
+    pass
